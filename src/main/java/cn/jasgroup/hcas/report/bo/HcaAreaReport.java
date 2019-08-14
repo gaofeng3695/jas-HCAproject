577798135b5ee1c5d813c3173775dc45620bbb2b
zhangyi <zhangyi@jasgroup.cn>
2019-08-14 18:16:33 +0800

    Feat: 增加高后果区报告生成功能

---
+++ src/main/java/cn/jasgroup/hcas/report/bo/HcaAreaReport.java
@@ -0,0 +1,20 @@
+package cn.jasgroup.hcas.report.bo;
+
+/**
+ * @description 地区等级报告
+ * @author zhangyi
+ * @date 2019年8月12日下午3:05:09
+ * @version V1.0
+ * @since JDK 1.80
+ */
+
+public class HcaAreaReport {
+
+	/**
+	 * 管道参数：名称、编号、起始里程、终止里程、长度、外管径、最大压强
+	 * 管道周边人口和自然环境情况
+	 * a.	地区等级划分统计表
+	 * b.	地区等级划分长度比例图
+	 * c.	再识别日期
+	 */
+}
